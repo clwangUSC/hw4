86d8a3f0280ba996bbdbaf3bcf38adbead758de7
Christina Wang <clwang@usc.edu>
2023-03-14 10:37:15 +0000

    finished equal paths

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,82 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
+int getHeight(Node *root)
+{
+	if (root == nullptr)
+	{
+		return 0;
+	}
+	int leftHeight = 1+getHeight(root->left);
+	int rightHeight = 1+getHeight(root->right);
+	int greatestHeight = leftHeight;
+	if(rightHeight>greatestHeight)
+	{
+		greatestHeight = rightHeight;
+	}
+	return greatestHeight;
+}
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
-
+    if(root == nullptr)
+    {
+        return true;
+    }
+    if(root->left == nullptr && root->right == nullptr)
+    {
+        return true;
+    }
+    if(root->left != nullptr)
+    {
+        if(root->left->left == nullptr && root->left->right == nullptr)
+        {
+            return (root->right == nullptr || (root->right->left==nullptr && root->right->right==nullptr));
+        }
+    }
+    if(root->right != nullptr)
+    {
+        if(root->right->left == nullptr&&root->right->right == nullptr)
+        {
+            return (root->left == nullptr || (root->left->left==nullptr && root->left->right==nullptr));
+        }
+    }
+    
+    /*
+    int leftHeight = getHeight(root->left);
+    int rightHeight = getHeight(root->right);
+    if(rightHeight!=leftHeight)
+    {
+      return false;
+    }
+    */
+    return (equalPaths(root->right)&&equalPaths(root->left));
 }
+
+
+
+
+/**bool treeComplete(Node* root)
+{
+    if(root == nullptr)
+    {
+        return true;
+    }
+    else if(root->left==nullptr && root->right==nullptr)
+    {
+        return true;
+    }
+    else if(root->left==nullptr && root->right!=nullptr)
+    {
+        return false;
+    }
+    else if(root->left!=nullptr && root->right==nullptr) 
+    {
+        return false;
+    }
+    else
+    {
+        return (equalPaths(root->right) && equalPaths(root->left));
+    }
+} **/
 
